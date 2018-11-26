e970608cc2c8ec689541ebf668f2357edb9f0c91
Jarek <jarektroyer@gmail.com>
2018-11-26 02:47:17 -0500

    Improved flow, added functionality

--- logic.js
+++ logic.js
@@ -2,20 +2,43 @@ $( document ).ready(function() {
   var count = 0;
   var goal = 0;
   var base_time = 0;
+  var time_tracker = 0;
   var timer;
+  var time_left;
 
+  //Checks for hydration goal
   function check_goal( count, goal ){
     return count >= goal;
   }
+
+  //Increments hydration count
   function closer(){
     count = count + 1;
   }
+
+  //Resets hydration trackers
   function reset(){
     count = 0;
     goal = 0;
     base_time = 0;
+    time_tracker = 0;
   }
 
+  //Returns a string representation of the time left in this round
+  function remaining_time(){
+    var duration = base_time - ( (new Date()).getTime() - time_tracker );
+    var seconds = parseInt((duration / 1000) % 60);
+    var minutes = parseInt((duration / (1000 * 60)) % 60);
+    var hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+    return hours + ":" + minutes + ":" + seconds;
+  }
+
+  //Checks for hydration success and cleans up from now-previous round
   function finish(){
     $("#param-wrapper").css('display', 'block');
     $("#watr-wrapper").css('display', 'none');
@@ -29,28 +52,39 @@ $( document ).ready(function() {
     reset();
   }
 
-  $("#success").css('display', 'none');
-  $("#failure").css('display', 'none');
-
+  //Hides achievment indicators on next round initiation
   $("input").click( function(){
     $("#success").css('display', 'none');
     $("#failure").css('display', 'none');
   });
 
+  //Hides our parameter entry and starts the next round
   $("#start-timing").click( function(){
     $("#param-wrapper").css('display', 'none');
 
     goal = $("#glasses").val();
     $("#glasses").val('');
 
-    base_time = ($("#duration").val()) * 3600;
+    base_time = ($("#duration").val()) * 3600 * 1000;
     $("#duration").val('');
 
-    timer = setTimeout( finish(), base_time );
+    time_tracker = (new Date()).getTime();
+
+    timer = setTimeout( finish, base_time );
   });
+
+  $("#reset").click( function(){
+    clearTimeout( timer );
+    finish();
+  });
+
+  //Makes sure our achievment indicators are hidden at the beginning
+  $("#success").css('display', 'none');
+  $("#failure").css('display', 'none');
+
+  //Updates the hydration timer
+  setInterval(function() {
+    $("#time").text( remaining_time() );
+  }, 1000);
+
 });
-
-
-
-
-var currentTime = new Date ();
